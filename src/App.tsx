6a727b286ff2c89e453dcd60200c4f040c78b751
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-20 17:46:54 +0500

    add to commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,12 @@ import {
 import {NavigationContainer} from "@react-navigation/native"
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 
+// screens
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+
+
 export default function App() {
   return (
     <View>
