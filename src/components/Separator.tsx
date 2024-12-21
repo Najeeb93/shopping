66387b208cc2f3bd45f09a2564345278307ee06a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-21 16:34:50 +0500

    add to commit

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import React from "react";
 
 
@@ -10,3 +10,5 @@ export default function Separator() {
     )
        
 }
+
+const styles = StyleSheet.create({});
