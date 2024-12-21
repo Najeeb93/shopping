53e7fca31430fd7e6d373a2f5d097848c67d3b9a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-21 16:48:19 +0500

    add to commit

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import React from 'react'
 
 
@@ -8,4 +8,6 @@ export default function ProductItem() {
             <Text>Product Item</Text>
         </View>
     )
-}
+};
+
+const style = StyleSheet.create({});
