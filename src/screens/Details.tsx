5d5b82fbcf1338eae006be2e4923ddeccf6cdf5e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-24 18:06:06 +0500

    add to commit

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
 import React from 'react'
 
 
@@ -12,5 +12,10 @@ const styles = StyleSheet.create({
     container: {
         paddingHorizontal: 18,
         backgroundColor: '#FFFFFF',
+    },
+    Image: {
+        width: 300,
+        height: 450,
+        resizeMode: 'contain',
     },
 })
