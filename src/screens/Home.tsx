12cb32a78350072ff11423ca66ad8720a7a9e185
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-24 17:49:07 +0500

    update

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,15 @@
 import {View, Text} from 'react-native'
 import React from 'react'
 
+//React navigation
+import {NativeStackScreenProps} from "@react-navigation/native-stack"
+import {RootStackPramList} from "../App"
+
+import ProductItem from '../components/ProductItem'
+import Separator from '../components/Separator'
+//data
+import { PRODUCTS_LIST } from '../data/constants'
+
 
 export default function Home() {
     return (
